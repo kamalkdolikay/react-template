bf0d14f33d7ccf0f7a6e32bbdaed35f8b1b60d86
Kamal Dolikay <kamaldolikay@gmail.com>
2025-08-26 01:12:37 -0700

    feat(feature): add home html

---
+++ src/features/home/components/home.html.tsx
@@ -0,0 +1,8 @@
+import './home.css';
+
+const HomeHtml = () => {
+  const greetingMessage = 'Hello, World!';
+  return <h1 className="hello-world">{greetingMessage}</h1>;
+};
+
+export default HomeHtml;
