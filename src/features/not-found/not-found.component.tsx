7040567a4b44dfa82341e9836604b69101aa2baa
Kamal Dolikay <kamaldolikay@gmail.com>
2025-08-26 01:22:35 -0700

    feat(not-found): add not-found component

---
+++ src/features/not-found/not-found.component.tsx
@@ -0,0 +1,7 @@
+import NotFoundHtml from './not-found.html';
+
+const NotFound = () => {
+  return <NotFoundHtml />;
+};
+
+export default NotFound;
---
+++ src/features/not-found/not-found.html.tsx
@@ -0,0 +1,12 @@
+import './not-found.css';
+
+const NotFoundHtml = () => {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '2rem' }}>
+      <h1>404 - Page Not Found</h1>
+      <p>Oops! The page you’re looking for doesn’t exist.</p>
+    </div>
+  );
+};
+
+export default NotFoundHtml;
